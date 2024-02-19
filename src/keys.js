aea7339bab74519efd6357d6dd3df06beb565317
_viktoria <viktorya.mazmanyan@debifi.com>
2024-02-20 01:03:32 +0400

    feat: serve

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 export const keys = {
-  api: "http://localhost:5001/api/v1",
-  // api: "http://154.49.137.44:5004/api/v1",
+  // api: "http://localhost:5001/api/v1",
+  api: "http://154.49.137.44:5004/api/v1",
   token: JSON.parse(localStorage.getItem("token")),
 };

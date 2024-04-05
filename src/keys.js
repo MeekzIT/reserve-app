63747b300fa0c3abcaf8b6c4bb40319ef0d91e66
_viktoria <viktorya.mazmanyan@debifi.com>
2024-04-05 10:45:49 +0400

    change config

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 export const keys = {
-  // api: "http://localhost:5001/api/v1",
-  api: "http://154.49.137.44:5000/api/v1",
-  token: JSON.parse(localStorage.getItem("token")),
-};
+	// api: "http://localhost:5000/api/v1",
+	api: "http://154.49.137.44:5000/api/v1",
+	token: JSON.parse(localStorage.getItem("token")),
+}
--- vite.config.js
+++ vite.config.js
@@ -1,68 +1,54 @@
-// import { defineConfig } from "vite";
-// import reactRefresh from "@vitejs/plugin-react";
-// import svgrPlugin from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react"
+import dns from "dns"
+import fs from "fs/promises"
+import { resolve } from "path"
+import { defineConfig } from "vite"
 
-// export default defineConfig({
-//   build: {
-//     outDir: "build",
-//   },
-//   plugins: [
-//     reactRefresh(),
-//     svgrPlugin({
-//       svgrOptions: {
-//         icon: true,
-//       },
-//     }),
-//   ],
-// });
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import fs from "fs/promises";
-import dns from "dns";
-
-dns.setDefaultResultOrder("verbatim");
+dns.setDefaultResultOrder("verbatim")
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      src: resolve(__dirname, "src"),
-    },
-  },
-  server: {
-    port: 3001,
-    host: "127.0.0.1",
-    hmr: {
-      clientPort: 3001,
-    },
-    proxy: {
-      '/api': {
-        target: 'http://154.49.137.44:5000',
-        changeOrigin: true,
-      },
-    },
-    // host: "154.49.137.44",
-  },
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        {
-          name: "load-js-files-as-jsx",
-          setup(build) {
-            build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({
-              loader: "jsx",
-              contents: await fs.readFile(args.path, "utf8"),
-            }));
-          },
-        },
-      ],
-    },
-  },
-  plugins: [react()],
-});
+	resolve: {
+		alias: {
+			src: resolve(__dirname, "src"),
+		},
+	},
+	build: {
+		outDir: "build",
+		assetsDir: "assets",
+		emptyOutDir: true,
+	},
+	server: {
+		port: 3001,
+		host: "127.0.0.1",
+		hmr: {
+			clientPort: 3001,
+		},
+		proxy: {
+			"/api": {
+				target: "http://154.49.137.44:5000",
+				changeOrigin: true,
+			},
+		},
+	},
+	esbuild: {
+		loader: "jsx",
+		include: /src\/.*\.jsx?$/,
+		exclude: [],
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			plugins: [
+				{
+					name: "load-js-files-as-jsx",
+					setup(build) {
+						build.onLoad({ filter: /src\\.*\.js$/ }, async args => ({
+							loader: "jsx",
+							contents: await fs.readFile(args.path, "utf8"),
+						}))
+					},
+				},
+			],
+		},
+	},
+	plugins: [react()],
+})

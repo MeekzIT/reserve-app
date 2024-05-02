da985e96b15def54879da83397d2d00e387ba42d
_viktoria <viktorya.mazmanyan@debifi.com>
2024-05-02 12:32:45 +0400

    update config

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 export const keys = {
 	// api: "http://localhost:5000/api/v1",
-	api: "http://154.49.137.44:5000/api/v1",
+	api: "https://reserve-back.jsxmachines.com/api/v1",
 	token: JSON.parse(localStorage.getItem("token")),
 }

2c3793c3c0681bae1de8e11a79c13dbee11863ec
_viktoria <viktorya.mazmanyan@debifi.com>
2024-05-15 14:44:24 +0400

    manifest

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 export const keys = {
-	api: "http://localhost:5000/api/v1",
-	// api: "https://reserve-back.jsxmachines.com/api/v1",
+	// api: "http://localhost:5000/api/v1",
+	api: "https://reserve-back.jsxmachines.com/api/v1",
 	token: JSON.parse(localStorage.getItem("token")),
 }
